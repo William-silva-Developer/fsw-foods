29abd538b4120a1913024851c4ddb6ecfb681f20
William-silva-Developer <william958silva@gmail.com>
2024-05-01 20:23:46 -0300

    feat: criação do product list e product item

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,28 @@ import Image from "next/image";
 import { Categorylist } from "./_components/category-list";
 import { Header } from "./_components/header";
 import { Search } from "./_components/search";
+import { ProductList } from "./_components/product-list";
+import { db } from "./_lib/prisma";
+import { Button } from "./_components/ui/button";
+import Link from "next/link";
+import { ChevronRightIcon } from "lucide-react";
 
-const Home = () => {
+const Home = async () => {
+  const products = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+    take: 10,
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+        },
+      },
+    },
+  });
   return (
     <>
       <Header />
@@ -24,6 +44,23 @@ const Home = () => {
           sizes="100vw"
           quality={100}
         />
+      </div>
+      <div className="space-y-4 pt-6">
+        <div className="flex items-center justify-between px-5">
+          <h2 className="font-semibold">Pedidos Recomendados</h2>
+
+          <Button
+            variant="ghost"
+            className="h-fit p-0 text-primary hover:bg-transparent"
+            asChild
+          >
+            <Link href="/products/recommended">
+              Ver todos
+              <ChevronRightIcon size={16} />
+            </Link>
+          </Button>
+        </div>
+        <ProductList products={products} />
       </div>
     </>
   );
